d571f52e9bacda31cf56c12fabf505a979eb9763
Brady Valentino <bradyvalentino@gmail.com>
2025-05-21 22:58:50 -0700

    Add a nice highlight to the sheet

--- PeapodApp/Custom UI/Modifiers/PPSheet.swift
+++ PeapodApp/Custom UI/Modifiers/PPSheet.swift
@@ -14,8 +14,15 @@ struct PPSheet: ViewModifier {
             content
                 .presentationCornerRadius(32)
                 .presentationDragIndicator(.visible)
-//                .presentationDetents([.height(500),.large])
                 .background(Color.background)
+        }
+        .overlay(alignment:.top) {
+            UnevenRoundedRectangle(cornerRadii: .init(topLeading: 32, bottomLeading: 0, bottomTrailing: 0, topTrailing: 32)).strokeBorder(Color.white.opacity(0.25), lineWidth:1)
+                .frame(height:64)
+                .mask(
+                    LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]),
+                                   startPoint: .top, endPoint: .bottom)
+                )
         }
     }
 }

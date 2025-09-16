2327fea397cf1e8db246d45685cfacbdb1cafe44
Brady Valentino <bradyvalentino@gmail.com>
2025-09-15 22:07:05 -0700

    Simplify longest episode stat

--- PeapodApp/Views/ActivityView.swift
+++ PeapodApp/Views/ActivityView.swift
@@ -168,38 +168,15 @@ struct ActivityView: View {
                     
                     if let longestEpisode = longestEpisode {
                         FadeInView(delay:0.3) {
-                            ZStack {
-                                KFImage(URL(string: longestEpisode.episodeImage ?? longestEpisode.podcast?.image ?? ""))
-                                    .resizable()
-                                    .frame(width:248,height:248)
-                                    .blur(radius:100)
-                                    .mask(
-                                        Image("rays")
-                                            .rotationEffect(Angle(degrees: rotationAngle))
-                                    )
-                                    .offset(y:-16)
-                                    .onAppear {
-                                        withAnimation(.linear(duration: 20).repeatForever(autoreverses: false)) {
-                                            rotationAngle = 360
-                                        }
-                                    }
-//                                Image("rays")
-//                                    .offset(y:-16)
-//                                    .rotationEffect(Angle(degrees: rotationAngle))
-//                                    .onAppear {
-//                                        withAnimation(.linear(duration: 20).repeatForever(autoreverses: false)) {
-//                                            rotationAngle = 360
-//                                        }
-//                                    }
-//                                    .mask(
-//                                        LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]),
-//                                                       startPoint: .top, endPoint: .bottom)
-//                                    )
+                            VStack {
+                                Text("Longest Completed Episode")
+                                    .titleCondensed()
+                                    .frame(maxWidth:.infinity,alignment:.leading)
                                 
-                                VStack {
-                                    Text("Longest Completed Episode")
-                                        .titleCondensed()
-                                        .frame(maxWidth:.infinity,alignment:.leading)
+                                HStack(spacing:16) {
+                                    Image(systemName: "laurel.leading")
+                                        .foregroundStyle(.yellow)
+                                        .font(.system(size: 32))
                                     
                                     VStack {
                                         HStack {
@@ -213,43 +190,24 @@ struct ActivityView: View {
                                             Text("\(formatDuration(seconds: duration))")
                                                 .textDetailEmphasis()
                                         }
-                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .frame(maxWidth: .infinity)
                                         
                                         VStack(alignment: .leading, spacing: 8) {
                                             Text(longestEpisode.title ?? "Episode title")
-                                                .multilineTextAlignment(.leading)
+                                                .multilineTextAlignment(.center)
                                                 .titleCondensed()
-                                            
-                                            Text(parseHtml(longestEpisode.episodeDescription ?? "Episode description", flat: true))
-                                                .multilineTextAlignment(.leading)
-                                                .textBody()
-                                                .lineLimit(3)
                                         }
                                         .frame(maxWidth: .infinity, alignment: .leading)
                                     }
-                                    .padding()
-                                    .background {
-                                        KFImage(URL(string: longestEpisode.episodeImage ?? longestEpisode.podcast?.image ?? ""))
-                                            .resizable()
-                                            .aspectRatio(contentMode:.fill)
-                                            .blur(radius:50)
-                                            .mask(
-                                                LinearGradient(gradient: Gradient(colors: [Color.black, Color.black.opacity(0)]),
-                                                               startPoint: .top, endPoint: .bottom)
-                                            )
-                                            .opacity(0.5)
-                                    }
-                                    .background(Color.background)
-                                    .clipShape(RoundedRectangle(cornerRadius:16))
-                                    .overlay {
-                                        LinearGradient(gradient: Gradient(colors: [Color.clear, Color.background]),
-                                                       startPoint: .init(x:0.5,y:0.5), endPoint: .init(x:0.5,y:1.0))
-                                        .frame(maxWidth:.infinity,maxHeight:.infinity)
-                                    }
+                                    
+                                    Image(systemName: "laurel.trailing")
+                                        .foregroundStyle(.yellow)
+                                        .font(.system(size: 32))
                                 }
-                                .frame(maxWidth:.infinity,alignment:.leading)
-                                .padding(.horizontal)
+                                
                             }
+                            .frame(maxWidth:.infinity,alignment:.leading)
+                            .padding(.horizontal)
                         }
                     }
                 }

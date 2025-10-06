ecd44ce16ec1cacab149b27e4f52e3d155e9c964
Brady Valentino <bradyvalentino@gmail.com>
2025-10-05 20:25:30 -0700

    Improve playback startup speed

--- PeapodApp/API/Managers/AudioPlayerManager.swift
+++ PeapodApp/API/Managers/AudioPlayerManager.swift
@@ -264,6 +264,7 @@ class AudioPlayerManager: ObservableObject, @unchecked Sendable {
         
         // Start playback - let system handle routing
         await MainActor.run {
+            self.player?.play()
             self.player?.rate = self.playbackSpeed
             
             episode.nowPlaying = true
